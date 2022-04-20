7951ac080c424aa49ce1ae8237c1560a4e5270a3
gwisnu <gunawan.wisnu@gmail.com>
2022-04-20 11:40:00 +0800

    update

--- tab2point.py
+++ tab2point.py
@@ -1,22 +1,26 @@
-def process_data(excel,area):
+def ptr2point(excel,area):
 
   if area == 'paringin':
+    mitra = 'BUMA'
     sheet = 'COAL BUMA'
     skip = 7
     column_no = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
     nama_field = ['date','class_unit','bdy_plan_weekly','bdy_plan_monthly','unit','seam_detail','seam','series','lp_n','lp_e','lp_z','dp_n','dp_e','dp_z','rom','access','highest','lowest','v_dist','st_dist','hz_dist','truck_count','rank']
   if area  == 'south':
     sheet='COAL BUMA'
+    mitra = 'BUMA'
     skip=8
     column_no = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,34]
     nama_field = ['date','class_unit','bdy_plan_weekly','bdy_plan_monthly','unit','seam_detail','seam','series','lp_n','lp_e','lp_z','dp_n','dp_e','dp_z','rom','access','highest','lowest','v_dist','st_dist','hz_dist','truck_count']
   if area  == 'wara':
     sheet='COAL WARA'
+    mitra = 'SIS'
     skip=5
     column_no = [0,1,2,3,4,5,6,7,8,9,10,11,12,15,17,18]
     nama_field = ['date','seam','series','bdy_plan','unit','lp_n','lp_e','lp_z','dp_n','dp_e','dp_z','rom','access','v_dist','hz_dist','truck_count']
   if area  == 'central':
     sheet='COAL CT'
+    mitra = 'SIS'
     skip=5
     column_no = [0,1,2,4,5,6,7,8,9,10,11,12,13,16,18,51]
     nama_field = ['date','seam','series','unit','lp_n','lp_e','lp_z','bdy_plan','dp_n','dp_e','dp_z','rom','access','v_dist','hz_dist','truck_count']
@@ -33,12 +37,13 @@ def process_data(excel,area):
   df_ptr['vol_vd'] = df_ptr['v_dist'] * df_ptr['truck_count']
   df_ptr['hz_dist_mtd'] = df_ptr['vol_hd'].cumsum()/df_ptr['truck_count'].cumsum()
   df_ptr['v_dist_mtd'] = df_ptr['vol_vd'].cumsum()/df_ptr['truck_count'].cumsum()
-
+  df_ptr['pit'] = area
+  df_ptr['mitra kerja'] = mitra
   last_update = df_ptr['date'].max().strftime('%y%m%d')
-  df_ptr[['date','unit','seam','series','lp_n','lp_e','lp_z','rom','access','v_dist','hz_dist','truck_count']].to_csv('coal_ptr_'+area+'_'+last_update+'.csv', index=True, index_label='id')
-  csvt_txt = "\"Integer\",\"Date\",\"String\",\"String\",\"String\",\"CoordY\",\"CoordX\",\"Real\",\"String\",\"String\",\"Real\",\"Real\",\"Real\""
+  df_ptr[['date','unit','seam','series','lp_n','lp_e','lp_z','rom','access','v_dist','hz_dist','truck_count','pit','mitra kerja']].to_csv('coal_ptr_'+area+'_'+last_update+'.csv', index=True, index_label='id')
+  csvt_txt = "\"Integer\",\"Date\",\"String\",\"String\",\"String\",\"CoordY\",\"CoordX\",\"Real\",\"String\",\"String\",\"Real\",\"Real\",\"Real\",\"String\",\"String\""
   csvt_file = open('coal_ptr_'+area+'_'+last_update+'.csvt', "w")
   n = csvt_file.write(csvt_txt)
   csvt_file.close()
   
-  print('Selesai..check files untuk download .csv dan .csvt')
+  print('Selesai, silakan cek file untuk download')
